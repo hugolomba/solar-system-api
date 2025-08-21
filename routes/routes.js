d4d5d516ecc308c5f2e05581474bd2414554da06
Hugo Lomba <98366477+hugolomba@users.noreply.github.com>
2025-08-21 11:06:12 +0100

    add function to normalize the user input

--- routes/routes.js
+++ routes/routes.js
@@ -12,17 +12,73 @@ const data = JSON.parse(fs.readFileSync(dataPath, "utf-8"));
 
 const routes = Router()
 
+const handleRequestResponse = (req, res) => {
+
+}
+
+function normalizeInput(input) {
+  return input
+    .normalize('NFD')              
+    .replace(/[\u0300-\u036f]/g, '') 
+    .replace(/\s+/g, '')             
+    .replace(/[^a-zA-Z0-9]/g, '')    
+    .toLowerCase();                 
+}
+
 routes.get("/", (req, res) => {
     res.json({
-  "/planets": "Listing all planets",
-  "/planet/:id": "Fetching a planet by its ID",
-  "/find/:string": "Compares each tag of each item to check if this string exists in the tags array, returning the objects that match."
-})
+        description: "Welcome to the Solar System API. This API provides detailed information about various celestial objects including planets, dwarf planets, asteroids, and galaxies. You can explore the full dataset or query specific categories and individual objects by their IDs.",
+        contents: [
+            "Planets: The major planets in our solar system.",
+            "Dwarf Planets: Smaller planetary bodies that orbit the sun.",
+            "Asteroids: Rocky objects primarily found in the asteroid belt.",
+            "Galaxies: Large systems of stars, dust, and dark matter."
+        ],
+        routes: {
+            "/all": "Retrieve the complete dataset including all categories.",
+            "/planets": "List all planets.",
+            "/planets/:id": "Get detailed information about a specific planet by its ID.",
+            "/dwarf-planets": "List all dwarf planets.",
+            "/asteroids": "List all asteroids.",
+            "/galaxies": "List all galaxies.",
+            "/find/:string": "Search for objects containing the specified string in their tags."
+        }
+    })
 })
 
+//show everything
+routes.get("/all", (req, res) => {
+    res.json(data)
+})
+
+// show the planets
 routes.get("/planets", (req, res) => {
     res.json(data.planets)
 })
+
+// show detailed info about a planet by name
+routes.get("/planet{s}/:name", (req, res) => {
+    const { id } = req.params
+    let filtered = data.planets.filter((p) => p.name.toLowerCase() === id)
+    res.json(filtered)
+    console.log(req.params);
+})
+
+//show dwarf planets
+
+// show detailed info about a dwarf planet by name
+
+
+// show planets + dwarf planets in the same list
+
+
+
+// show asteroids
+
+// show galaxies
+
+// /find/:string search for objects containing the specified string in their tags
+
 
 routes.get("/planets/:id", (req, res)=> {
     console.log(req.params)
